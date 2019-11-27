fcd021361f86f1497bf9d1d69ba4e8419bf82bb0
Sultan Karimov <skarimovkgz@gmail.com>
2019-11-27 00:22:59 -0600

    starting point

---
+++ src/resumeData.js
@@ -0,0 +1,137 @@
+let resumeData = {
+    "imagebaseurl":"www.linkedin.com/in/sultankarimov",
+    "name": "Sultanmurat Karimov",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of front-end and back-end development and like to learn about new technologies. Also like to do video editing or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sultankarimov",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Sultan94k",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I'm able to handle multiple tasks on a daily basis and like to think out of the box, build things. I became very interested in startup's tecknology and learning how to code. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Chicago IL",
+    "website":"www.linkedin.com/in/sultankarimov",
+    "education":[
+      {
+        "UniversityName":"Northwestern University Coding Bootcamp",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Nov",
+        "YearOfPassing":"2019",
+        "Achievements":"JavaScript, Html, CSS, Jquery, React, MySql, MongoDB, Express etc."
+      },
+      {
+        "UniversityName":"CSI Computer Systems Institute",
+        "specialization":"AAP Administrative Asisstant Professional",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2016",
+        "Achievements":"Customer service, Accounting, Bookkeeping, Time managment etc. "
+      },
+      {
+        "UniversityName":"IUK International University of Kyrgyzstan",
+        "specialization":"IT Computer Science",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2013",
+        "Achievements":"C++"
+      }
+
+
+    ],
+    "work":[
+      {
+        "CompanyName":"KGZ Trans Inc",
+        "specialization":"Small Business owner",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"KGZ Transport Corp",
+        "specialization":"Fleet Manager",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2016",
+        "Achievements":"Provides Deliveries that take goods from one place to another on behalf of a shipper"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname": "HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname": "JavaScript"
+      },
+      {
+        "skillname": "Reactjs"
+      },
+      {
+        "skillname":"Sequlize"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"MySql"
+      },
+      {
+        "skillname":"jQuery"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Trivia-Game",
+        "description":"mobileapp",
+        "imgurl": "images/portfolio/TriviaGAme.png",
+        "url":"Demo",
+      },
+      {
+        "name":"Friend Finder",
+        "description":"mobileapp",
+        "imgurl": "images/portfolio/FriendFinder.png",
+        "url1": "Demo"
+      },
+      {
+        "name":"Clicky Game",
+        "description":"mobileapp",  
+        "imgurl": "images/portfolio/ClickyGame.png",
+        "url":"https://salty-island-17838.herokuapp.com/"
+      },
+      {
+        "name":"Weather-API",
+        "description":"mobileapp",
+        "imgurl": "images/portfolio/weather.png",
+        "url":"https://sultan94k.github.io/WeatherAPI/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
